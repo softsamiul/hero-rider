2ed4220e324fd859352d1f4102cecc05173f5d0a
softsamiul <softsamiul.com>
2021-12-31 14:10:14 +0530

    react-router-dom v5 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import SignIn from './Pages/SignIn/SignIn';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/home'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/signup'>
+            <SignUp></SignUp>
+          </Route>
+          <Route exact path='/signin'>
+            <SignIn></SignIn>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
